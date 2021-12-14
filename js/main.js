a76dee41e0e424d7c7214fc6f435deed258df2d2
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2021-12-14 17:18:21 +0200

    checkpoint

--- js/main.js
+++ js/main.js
@@ -4,30 +4,36 @@ const percentage = document.getElementById('select-percentage');
 const btn = document.getElementById('btn-convert');
 const outputCss = document.getElementById('output-css');
 
+animationDurationText.value = '5';
 btn.addEventListener('click', getCssRules);
 
 function getCssRules() {
   const text = inputText.value;
-  let textCutIterable = 0;
-  let textCutIndex = 1;
-  let innerCssRule = '';
+  if (text) {
+    inputText.style.cssText = '';
+    let textCutIterable = 0;
+    let textCutIndex = 0;
+    let innerCssRule = '';
 
-  let selectedPercentage = +percentage.value;
-  for (let i = selectedPercentage; i <= 100; i += selectedPercentage) {
-    innerCssRule += `
+    let selectedPercentage = +percentage.value;
+    for (let i = selectedPercentage; i <= 100; i += selectedPercentage) {
+      innerCssRule += `
     ${i}% {
         content: '${text.slice(0, textCutIndex)} |';
     }
 `;
-    textCutIterable++;
-    if (textCutIterable == text.length * 2 - 1) {
-      textCutIterable = 0;
-    } else {
-      textCutIndex = textCutIterable <= text.length ? textCutIterable : text.length * 2 - textCutIterable;
-    }
-  }
+      textCutIterable++;
 
-  outputCss.value = `
+      if (textCutIterable == text.length * 2) {
+        textCutIterable = 1;
+      } else {
+        textCutIndex = textCutIterable <= text.length ? textCutIterable : text.length * 2 - textCutIterable;
+      }
+
+      //  console.log(`text.length ${text.length}`, `textCutIndex ${textCutIndex}`, `textCutIterable ${textCutIterable}`);
+    }
+
+    outputCss.value = `
   .output-animation {
       animation: typing-text ${animationDurationText.value}s ease-in-out 0s infinite alternate both;
     }
@@ -37,4 +43,7 @@ function getCssRules() {
     }
 
   `;
+  } else {
+    inputText.style.cssText = 'border-color: red;box-shadow: 0 0 0 0.25rem red;';
+  }
 }
